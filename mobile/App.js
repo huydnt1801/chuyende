63997840c16f2230961a7848c1df6bf78739f66c
phutq0 <phutq0@gmail.com>
2023-04-12 23:57:37 +0700

    Add language, store

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { } from "nativewind"
+import { } from "nativewind";
+import { I18nextProvider } from "react-i18next";
+import { Provider } from "react-redux"
 
 import Home from "./src/screens/Home";
 import Login from "./src/screens/Login";
 import Mapp from "./src/screens/Mapp";
+import store from "./src/store";
 
 const Stack = createNativeStackNavigator();
 
@@ -17,27 +19,22 @@ const screens = [
 
 const App = () => {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                {screens.map(item => (
-                    <Stack.Screen
-                        key={item.name}
-                        name={item.name}
-                        component={item.component}
-                        options={{ headerShown: false }} />
-                ))}
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Provider store={store}>
+            <I18nextProvider>
+                <NavigationContainer>
+                    <Stack.Navigator>
+                        {screens.map(item => (
+                            <Stack.Screen
+                                key={item.name}
+                                name={item.name}
+                                component={item.component}
+                                options={{ headerShown: false }} />
+                        ))}
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </I18nextProvider>
+        </Provider>
     );
 }
 
 export default App
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
