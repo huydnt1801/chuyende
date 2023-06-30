3f49c1b6420005c3352e27903472277b2f769c98
huydnt1801 <tienhuycodeforces@gmail.com>
2023-06-30 21:54:47 +0700

    use accessToken

--- server/api/server/account.go
+++ server/api/server/account.go
@@ -15,7 +15,6 @@ import (
 	"github.com/huydnt1801/chuyende/api/server/middleware/auth"
 	"github.com/huydnt1801/chuyende/internal/config"
 	"github.com/huydnt1801/chuyende/internal/driver"
-	"github.com/huydnt1801/chuyende/internal/session"
 	"github.com/huydnt1801/chuyende/internal/user"
 	"github.com/huydnt1801/chuyende/pkg/log"
 	"github.com/labstack/echo/v4"
@@ -26,7 +25,6 @@ type AccountServer struct {
 	secretKey string
 
 	userSvc   user.Service
-	sessSvc   session.SessionService
 	driverSvc driver.Service
 }
 
@@ -37,7 +35,6 @@ func NewAccountServer(db *sql.DB) *AccountServer {
 		logger:    log.ZapLogger(),
 		secretKey: cfg.SecretKey,
 		userSvc:   svc,
-		sessSvc:   session.NewSessionService(db),
 		driverSvc: driver.NewService(db),
 	}
 	return srv
@@ -135,13 +132,6 @@ type RegisterConfirmResponse struct {
 }
 
 func (s *AccountServer) Logout(c echo.Context) error {
-	r := c.Request()
-	ctx := r.Context()
-	authInfo, _ := auth.GetAuthInfo(c)
-	err := s.sessSvc.RevokeSession(ctx, authInfo.SessionID)
-	if err != nil {
-		return err
-	}
 	auth.LogoutUser(c)
 	return c.JSON(http.StatusOK, LogoutResponse{Code: http.StatusOK, Data: nil})
 }
@@ -154,10 +144,6 @@ type LogoutResponse struct {
 func (s *AccountServer) Login(c echo.Context) error {
 	r := c.Request()
 	ctx := r.Context()
-	authInfo, loggedIn := auth.GetAuthInfo(c)
-	if loggedIn {
-		return c.JSON(http.StatusOK, LoginResponse{Code: http.StatusOK, Data: authInfo.User})
-	}
 	data := &LoginRequest{}
 	if err := c.Bind(data); err != nil {
 		return err
@@ -169,11 +155,9 @@ func (s *AccountServer) Login(c echo.Context) error {
 	if err != nil {
 		return err
 	}
-	sessID, err := s.sessSvc.CreateSession(ctx, usr.ID, 0)
-	if err != nil {
+	if err := auth.LoginUser(c, usr.ID, 0); err != nil {
 		return err
 	}
-	auth.LoginUser(c, sessID, usr, nil)
 	return c.JSON(http.StatusOK, LoginResponse{Code: http.StatusOK, Data: usr})
 }
 
@@ -198,7 +182,7 @@ func (s *AccountServer) UpdateInfo(c echo.Context) error {
 		return err
 	}
 	authInfo, loggedIn := auth.GetAuthInfo(c)
-	if loggedIn && authInfo.User.ID != data.UserID {
+	if loggedIn && authInfo.UserID != data.UserID {
 		return echo.NewHTTPError(http.StatusForbidden, "Bạn không có quyền chỉnh sửa")
 	}
 	usr, err := s.userSvc.UpdateUser(ctx, &user.UserUpdate{
@@ -239,11 +223,7 @@ func (s *AccountServer) LoginDriver(c echo.Context) error {
 	if err != nil {
 		return err
 	}
-	sessID, err := s.sessSvc.CreateSession(ctx, 0, driv.ID)
-	if err != nil {
-		return err
-	}
-	auth.LoginUser(c, sessID, nil, driv)
+	auth.LoginUser(c, 0, driv.ID)
 	return c.JSON(http.StatusOK, LoginDriverResponse{Code: http.StatusOK, Data: driv})
 }
 
--- server/api/server/account_integration_test.go
+++ server/api/server/account_integration_test.go
@@ -5,9 +5,17 @@ package server
 
 import (
 	"context"
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
 	"github.com/huydnt1801/chuyende/internal/ent"
+	"github.com/huydnt1801/chuyende/internal/user"
+	"github.com/huydnt1801/chuyende/test"
+	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -16,23 +24,24 @@ const (
 
 func mockUsers(client *ent.Client, len int) []*ent.User {
 	var ret []*ent.User
+	passwordHasher := &user.BcryptPasswordHasher{}
 	for idx := 1; idx <= len; idx++ {
 		user := &ent.User{
 			PhoneNumber: "012345678" + fmt.Sprint(idx),
 			Confirmed:   true,
 			FullName:    "test user " + fmt.Sprint(idx),
-			Password:    "testuser",
+			Password:    "123456",
 		}
 		ret = append(ret, user)
 	}
-
 	bulk := make([]*ent.UserCreate, 0)
 	for _, user := range ret {
+		hashPass, _ := passwordHasher.HashPassword(user.Password)
 		q := client.User.Create().
 			SetFullName(user.FullName).
 			SetConfirmed(user.Confirmed).
 			SetPhoneNumber(user.PhoneNumber).
-			SetPassword(user.Password)
+			SetPassword(hashPass)
 		bulk = append(bulk, q)
 	}
 
@@ -64,4 +73,67 @@ func mockDrivers(client *ent.Client, len int) []*ent.VehicleDriver {
 	mockData, _ := client.VehicleDriver.CreateBulk(bulk...).
 		Save(context.Background())
 	return mockData
+}
+
+type TestLoginInfo struct {
+	body   map[string]interface{}
+	output *LoginResponse
+}
+
+func TestLogin(t *testing.T) {
+	// Create env
+	env := test.NewTestEnv(t, container, "TestLogin")
+	assert.NotNil(t, env)
+
+	client := env.Client
+	db := env.Database
+
+	// Mock mysql data
+	mockUsers := mockUsers(client, 1)
+
+	tests := []struct {
+		name string
+		info *TestLoginInfo
+	}{
+		{
+			name: "[TestLogin][Success] Return 200 - Return login success",
+			info: &TestLoginInfo{
+				body: map[string]interface{}{
+					"phoneNumber": mockUsers[0].PhoneNumber,
+					"password":    "123456",
+				},
+				output: &LoginResponse{
+					Code: http.StatusOK,
+					Data: &user.User{
+						ID: 1,
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			doLogin(t, tc.info, db)
+		})
+	}
+}
+
+func doLogin(t *testing.T, info *TestLoginInfo, db *sql.DB) {
+	req := test.BuildPostQuery(AccountEndpoint+"/login", info.body)
+	rec := httptest.NewRecorder()
+	c := NewTestEchoContext().NewContext(req, rec)
+
+	accountSv := NewAccountServer(db)
+	err := accountSv.Login(c)
+
+	if info.output.Code == http.StatusOK {
+		assert.Nil(t, err)
+		var actual *LoginResponse
+		err := json.NewDecoder(rec.Body).Decode(&actual)
+		assert.Nil(t, err)
+		assert.Equal(t, info.output.Code, actual.Code)
+	} else {
+		assert.Equal(t, info.output.Code, GetErrorCode(err))
+	}
 }
