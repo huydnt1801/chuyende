0ae55117ffaf707419c1214b360f67f20c4bcf43
hoduchan <hoduchan38@gmail.com>
2023-07-03 00:46:30 +0700

    voucher screen

---
+++ mobile/src/screens/Home/pages/Reward/className.js
@@ -0,0 +1,11 @@
+const className = {
+    wrapper: `w-full h-14 border-b border-gray-300`,
+    left: `absolute left-0 top-0 bottom-0 z-[1] items-center justify-center w-14`,
+    center: `flex-1 items-center flex-row pl-5 bg-white `,
+    title: `font-bold text-lg`,
+    right: `absolute right-0 top-0 bottom-0 z-[1] items-center justify-center pr-3`,
+    container: `flex-1 bg-gray-100`,
+    evoucher: `w-full h-30 bg-white`,
+}
+
+export default className
